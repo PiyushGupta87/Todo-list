7629800a577b7955291060ce1ba7b383a208a3a2
PiyushGupta87 <pk5414696@gmail.com>
2026-02-19 12:55:07 +0530

    Add files via upload

---
+++ Todo-list/script.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box");
+const listContiner = document.getElementById("list-continer");
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert("You must write something");
+    }else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContiner.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+listContiner.addEventListener("click",function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("chekced");
+        saveData();
+    }else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listContiner.innerHTML);
+}
+function showTask(){
+    listContiner.innerHTML = localStorage.getItem("data");
+}
+showTask();
